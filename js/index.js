7119fc8ef9e90a6ccd7f47342cc910712a49729f
Simon Jeong <simon@myika.co>
2021-05-12 13:45:25 +0900

    spelling mistake

--- js/index.js
+++ js/index.js
@@ -68,7 +68,7 @@ function connectWs() {
             newA.push(newData)
           })
           candlestickDisplayData.forEach(oldData => {
-            oldDate.DateTime = new Date(Math.abs(oldData.DateTime) + getLocalTimezone()).toISOString().split(".")[0]
+            oldData.DateTime = new Date(Math.abs(oldData.DateTime) + getLocalTimezone()).toISOString().split(".")[0]
             // newA.push(oldData)
             console.log(oldData)
           })
